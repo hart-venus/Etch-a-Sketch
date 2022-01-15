634428afcc52fdb08d6b85218976034983ee83ec
Ariel Leyva <aleyva1509@gmail.com>
2022-01-15 17:36:38 -0600

    sketch works now!!!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let resolution = 100;
+let resolution = 16;
 const container = document.getElementById('container')
+
 for(let i = 0; i <resolution;i++){
     let subContainer = document.createElement('div')
     subContainer.classList.add('subcontainer')
@@ -13,6 +14,15 @@ for(let i = 0; i <resolution;i++){
     container.appendChild(subContainer)
 
 }
+
+document.querySelectorAll('.gridItem').forEach(item => {
+    item.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black'
+    })
+})
+
+
+
 /*
 for(let i = 0; i <resolution;i++){
     let gridItem = document.createElement('div')
