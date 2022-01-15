86c0739814644001388ff7a4602690fe709feb6b
Ariel Leyva <aleyva1509@gmail.com>
2022-01-15 17:10:30 -0600

    generating divs via code

---
+++ script.js
@@ -0,0 +1,11 @@
+let resolution = 16;
+const container = document.getElementById('container')
+for(let i = 0; i <resolution;i++){
+    for(let i = 0; i <resolution;i++){
+        let gridItem = document.createElement('div')
+        gridItem.textContent = 'div'
+        container.appendChild(gridItem)
+   
+    }
+   
+}
