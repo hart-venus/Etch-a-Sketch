acf852057e29ee2d2103702376095104a96b8b5f
Ariel Leyva <aleyva1509@gmail.com>
2022-01-15 17:24:50 -0600

    basic grid system

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let resolution = 16;
+let resolution = 100;
 const container = document.getElementById('container')
 for(let i = 0; i <resolution;i++){
     let subContainer = document.createElement('div')
-    subContainer.textContent = 'div'
+    subContainer.classList.add('subcontainer')
+    for(let i = 0; i<resolution;i++){
+        let gridItem = document.createElement('div')
+        gridItem.classList.add('gridItem')
+        subContainer.appendChild(gridItem)
+    }
+
+
     container.appendChild(subContainer)
 
 }
