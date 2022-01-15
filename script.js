c28a016a1dd93f8d4f3f05cd44b70464653cd0ca
Ariel Leyva <aleyva1509@gmail.com>
2022-01-15 17:50:44 -0600

    fixed bugs and added generating

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-let resolution = 16;
+
 const container = document.getElementById('container')
+const clear = document.getElementById('clear')
 
-for(let i = 0; i <resolution;i++){
-    let subContainer = document.createElement('div')
-    subContainer.classList.add('subcontainer')
-    for(let i = 0; i<resolution;i++){
-        let gridItem = document.createElement('div')
-        gridItem.classList.add('gridItem')
-        subContainer.appendChild(gridItem)
+function generate(res){
+    for(let i = 0; i <res;i++){
+        let subContainer = document.createElement('div')
+        subContainer.classList.add('subcontainer')
+        for(let i = 0; i<res;i++){
+            let gridItem = document.createElement('div')
+            gridItem.classList.add('gridItem')
+            subContainer.appendChild(gridItem)
+        }
+        
+        
+    
+        container.appendChild(subContainer)
+    
     }
-
-
-    container.appendChild(subContainer)
-
+    document.querySelectorAll('.gridItem').forEach(item => {
+        item.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black'
+        })
+    })
 }
 
-document.querySelectorAll('.gridItem').forEach(item => {
-    item.addEventListener('mouseover', function(event){
-        event.target.style.backgroundColor = 'black'
-    })
+//generate(parseInt(prompt('how many squares you fucker')))
+
+clear.addEventListener('click', () => {
+    generate(12)
 })
+
+
 
 
 
