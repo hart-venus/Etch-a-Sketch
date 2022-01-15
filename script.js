a8f523f39aa41bde6da22df04b966b982efbbfd0
Ariel Leyva <aleyva1509@gmail.com>
2022-01-15 17:16:15 -0600

    some magic

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let resolution = 16;
 const container = document.getElementById('container')
 for(let i = 0; i <resolution;i++){
-    for(let i = 0; i <resolution;i++){
-        let gridItem = document.createElement('div')
-        gridItem.textContent = 'div'
-        container.appendChild(gridItem)
-   
-    }
-   
+    let subContainer = document.createElement('div')
+    subContainer.textContent = 'div'
+    container.appendChild(subContainer)
+
 }
+/*
+for(let i = 0; i <resolution;i++){
+    let gridItem = document.createElement('div')
+    gridItem.textContent = 'div'
+    container.appendChild(gridItem)
+    console.log(i)
+}
+*/
